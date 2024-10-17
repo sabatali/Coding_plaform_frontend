8b10bc8c29f46906dc35851c45c1cf1a10847f33
Sabat Ali <126473010+sabatali@users.noreply.github.com>
2024-10-17 19:15:48 +0500

    Update Dashboard.jsx

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -1,46 +1,57 @@
-
 import { useContext } from "react";
 import { TableContext } from "../Context/TableContext";
 import QuestionTable from "../tables/questionTable";
 import QuickStackCard from "./QuickStackCard";
 import {
   UserGroupIcon,
-  ShoppingCartIcon,
-  ColorSwatchIcon,
-  CubeIcon,
-} from "@heroicons/react/outline";
+  DocumentTextIcon,
+  FolderIcon,
+  UsersIcon,
+} from "@heroicons/react/outline"; // Updated Icons
 
 function Dashboard() {
   const { tableData } = useContext(TableContext);
-
-
-  const loading = true;
+  const loading = false; // For testing, we assume loading is false now
 
   const iconClass = "w-10 h-10 text-gray-400";
+
   return (
     <div className="flex flex-col space-y-6">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+        {/* Total Questions */}
         <QuickStackCard title="Total Questions" statics={tableData.length}>
-          <CubeIcon className={iconClass} />
+          <DocumentTextIcon className={iconClass} />
         </QuickStackCard>
-        {!loading ? ( <>
-          <QuickStackCard title="Today Questions" statics={"00"}>
-          <UserGroupIcon className={iconClass} />
-        </QuickStackCard>
-        </> ) : ( <> <QuickStackCard title="Today Posts" statics={"00"}>
-          <UserGroupIcon className={iconClass} />
-        </QuickStackCard></>)}
 
-        <QuickStackCard title="Total Fliles" statics={"00"}>
-          <ColorSwatchIcon className={iconClass} />
+        {/* Today Questions or Today Posts */}
+        {!loading ? (
+          <>
+            <QuickStackCard title="Today Questions" statics={"00"}>
+              <DocumentTextIcon className={iconClass} />
+            </QuickStackCard>
+          </>
+        ) : (
+          <>
+            <QuickStackCard title="Today Posts" statics={"00"}>
+              <DocumentTextIcon className={iconClass} />
+            </QuickStackCard>
+          </>
+        )}
+
+        {/* Total Files */}
+        <QuickStackCard title="Total Files" statics={"00"}>
+          <FolderIcon className={iconClass} />
         </QuickStackCard>
-      
-        <QuickStackCard title="Total User" statics="00">
-          <ShoppingCartIcon className={iconClass} />
+
+        {/* Total Users */}
+        <QuickStackCard title="Total Users" statics="00">
+          <UsersIcon className={iconClass} />
         </QuickStackCard>
       </div>
+
+      {/* Question Table */}
       <div className="">
-        < QuestionTable/>
+        <QuestionTable />
       </div>
     </div>
   );
