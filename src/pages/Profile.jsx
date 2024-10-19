d6dc700c236ee49b27869b3c6c8e87e3a2cee276
sabatali <maliksabatali@gmail.com>
2024-10-19 21:39:24 +0500

    update

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,21 +3,26 @@ import { AuthContext } from '../Context/authContext';
 import { toast, ToastContainer } from 'react-toastify';
 import axios from 'axios';
 import { local_url } from '../constent';
+import DotsLoader from '../Components/DotsLoader/DotsLoader';
 
 const ProfilePage = () => {
     const { userData } = useContext(AuthContext);
+    const [loading , setLoading] = useContext(false)
     
 
     console.log("🚀 ~ ProfilePage ~ userData:", userData)
 
     const handleUpdateUser = async (id) => {
+      setLoading(true)
       try {
         const res = await axios.post(`${local_url}/api/v1/adminmail/${id}`);
   
         console.log("🚀 ~ handleDelete ~ res:", res)
         toast.success(res.data.message || "Request Sended");
+        setLoading(false)
       } catch (error) {
         console.error("Failed to send request:", error);
+        setLoading(false)
         toast.error( 
        "Failed to send request ");
       }
@@ -78,15 +83,23 @@ const ProfilePage = () => {
             <p className="text-gray-600">Joined:</p>
             <p className="text-indigo-600">{new Date(userData.createdAt).toDateString()}</p>
           </div>
-
-          {!(userData.role == "contributor") &&
-          <button onClick={() =>  handleUpdateUser(userData._id)} class="relative inline-flex items-center justify-center p-4 px-5 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-full shadow-xl group hover:ring-1 hover:ring-purple-500">
-          <span class="absolute inset-0 w-full h-full bg-gradient-to-br from-blue-600 via-purple-600 to-pink-700"></span>
-          <span class="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-45 translate-x-24 bg-pink-500 rounded-full opacity-30 group-hover:rotate-90 ease"></span>
-          <span class="relative text-white">Become Contributor</span>
-          </button>
-
-          }
+          {!loading ? (
+  userData.role !== "contributor" && (
+    <button onClick={() => handleUpdateUser(userData._id)} className="relative inline-flex items-center justify-center p-4 px-5 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-full shadow-xl group hover:ring-1 hover:ring-purple-500">
+      <span className="absolute inset-0 w-full h-full bg-gradient-to-br from-blue-600 via-purple-600 to-pink-700"></span>
+      <span className="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-45 translate-x-24 bg-pink-500 rounded-full opacity-30 group-hover:rotate-90 ease"></span>
+      <span className="relative text-white">Become Contributor</span>
+    </button>
+  )
+) : (
+  userData.role !== "contributor" && (
+    <button className="relative inline-flex items-center justify-center p-4 px-5 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-full shadow-xl group hover:ring-1 hover:ring-purple-500">
+      <span className="absolute inset-0 w-full h-full bg-gradient-to-br from-blue-600 via-purple-600 to-pink-700"></span>
+      <span className="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-45 translate-x-24 bg-pink-500 rounded-full opacity-30 group-hover:rotate-90 ease"></span>
+      <span className="relative text-white"><DotsLoader /></span>
+    </button>
+  )
+)}
 
 
         </div>
