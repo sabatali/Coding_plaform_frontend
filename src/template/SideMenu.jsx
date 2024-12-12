3ef2c5c6223f85db20112b0d82f582eaa4ee28e8
Sabat Ali <126473010+sabatali@users.noreply.github.com>
2024-12-12 16:32:17 +0500

    Update SideMenu.jsx

--- src/template/SideMenu.jsx
+++ src/template/SideMenu.jsx
@@ -24,9 +24,11 @@ function Header(props) {
       className="flex lg:space-x-3 justify-center lg:justify-start lg:px-3 border-b border-gray-900 items-center bg-gray-800 shadow-md"
       style={{ height: headerHeight }}
     >
-      <h2 className="text-white text-2xl font-semibold hidden px-4 lg:inline">
+     <img src="public/code-p.png" alt="Code P" />
+
+     {/* <h2 className="text-white text-2xl font-semibold hidden px-4 lg:inline">
         {props.title}
-      </h2>
+      </h2> */}
     </div>
   );
 }
